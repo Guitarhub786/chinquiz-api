40c433acf1f9f8a7f589a72d55601621980fdf36
Guitarhub786 <codewithdrawal@gmail.com>
2020-04-10 22:21:25 +0100

    Starter server.js

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/api/chinquiz', (req, res) => {
+  const chinquiz = [
+    {
+      question:
+        "Which country, not including Japan, has the most people of japanese decent?",
+      answers: ["Brazil", "China", "South Korea", "United States of America"],
+      correct: "Brazil",
+      questionId: "4795960"
+    },
+    {
+      question: "Which candy is NOT made by Mars?",
+      answers: ["Almond Joy", "M&M's", "Twix", "Snickers"],
+      correct: "Almond Joy",
+      questionId: "4811162"
+    },
+    {
+      question: "In which fast food chain can you order a Jamocha Shake?",
+      answers: ["Arby's", "McDonald's", "Burger King", "Wendy's"],
+      correct: "Arby's",
+      questionId: "4982981"
+    }
+  ]
+
+  res.json(chinquiz);
+});
+
+const port = 5000;
+
+app.listen(port, () => `Chinquiz Server running on port ${port}`);
